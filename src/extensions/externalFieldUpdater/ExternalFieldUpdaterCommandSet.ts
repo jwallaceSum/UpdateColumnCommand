ce4756efaae2a05aee8ae5ecab63ffabeabb6785
jwallaceSum <79501838+jwallaceSum@users.noreply.github.com>
2022-11-04 17:28:22 +1100

    updates

--- src/extensions/externalFieldUpdater/ExternalFieldUpdaterCommandSet.ts
+++ src/extensions/externalFieldUpdater/ExternalFieldUpdaterCommandSet.ts
@@ -5,7 +5,8 @@ import {
   Command,
   IListViewCommandSetListViewUpdatedParameters,
   IListViewCommandSetExecuteEventParameters,
-  RowAccessor
+  RowAccessor,
+  ListItemAccessor
 } from '@microsoft/sp-listview-extensibility';
 import { BaseDialog, Dialog } from '@microsoft/sp-dialog';
 import * as strings from 'ExternalFieldUpdaterCommandSetStrings';
@@ -25,7 +26,8 @@ import { List } from '@pnp/sp/lists';
 import { Batch } from '@pnp/odata';
 import { JSONParser } from "@pnp/odata";
 import { IFile } from '@pnp/sp/files/types';
-
+import OptionDialog from './OptionsDialog';
+import { IDropdownOption } from 'office-ui-fabric-react';
 
 
 /**
@@ -47,16 +49,16 @@ export interface IExternalFieldUpdaterCommandSetProperties {
 
 const LOG_SOURCE: string = 'ExternalFieldUpdaterCommandSet';
 
-
 export default class ExternalFieldUpdaterCommandSet extends BaseListViewCommandSet<IExternalFieldUpdaterCommandSetProperties> {
   private isInOwnersGroup: boolean = false;
+  private _selectedKey: IDropdownOption;
   @override
   public async onInit(): Promise<void> {
-
     await super.onInit();
-    let user = await sp.web.currentUser();
+    let groups = await sp.web.currentUser.groups.get();
     await sp.setup({ spfxContext: this.context });
-    this.isInOwnersGroup = user.IsSiteAdmin;
+    this.isInOwnersGroup = true;
+    console.log(this.isInOwnersGroup);
 
     return Promise.resolve<void>();
   }
@@ -66,75 +68,101 @@ export default class ExternalFieldUpdaterCommandSet extends BaseListViewCommandS
     this.tryGetCommand('COMMAND_1').visible = this.isInOwnersGroup && (event.selectedRows.length >= 1);
   }
 
-  private async updateFile(itemID: any, list: any){
+  private async updateFile(itemID: any, list: any, batch: SPBatch){
     const entityTypeFullName = await list.getListItemEntityTypeFullName();
     const parser = new JSONParser();
     let fileType = await list.items.getById(itemID).get(parser);
-    let currentValue = await list.items.getById(itemID).select('ExternalSite').get(parser);
-    currentValue = currentValue.ExternalSite;
-    let newValue= null;
+    let newValue =(this._selectedKey.key != 'No');
     if(fileType.FileSystemObjectType == 1){
-      console.log('Folder: ');
-      console.log(fileType);
+     console.log('Folder: ');
+     console.log(fileType);
       let files = await list.items.getById(itemID).folder.files();
       let folders = await list.items.getById(itemID).folder.folders();
-      console.log(files);
-      console.log(folders);
+      //console.log(files);
+      //console.log(folders);
       for(let i in files) {
-        console.log('File:');
-        console.log(files[i]);
+        //console.log('File:');
+        //console.log(files[i]);
         let url = files[i].ServerRelativeUrl;
-        console.log(url);
+        //console.log(url);
         let file = await sp.web.getFileByServerRelativeUrl(url).getItem();
         let id = file['Id'];
         console.log('List Id:'+ id);
-        await this.updateFile(id, list);
+        // batch = await this.updateFile(id, list, batch);
+        list.items.getById(id).inBatch(batch).update({ ExternalSite: newValue }, "*", entityTypeFullName).then(b => {
+          console.log(b);
+        });
       }
       for(let i in folders) {
-        console.log('Folder:');
-        console.log(folders[i]);
+        //console.log('Folder:');
+        //console.log(folders[i]);
         let url = folders[i].ServerRelativeUrl;
-        console.log(url);
+        // console.log(url);
         let folder = await sp.web.getFolderByServerRelativeUrl(url).getItem();
-        console.log(folder);
+        // console.log(folder);
         let id = folder['Id'];
         console.log('List Id:'+ id);
-        await this.updateFile(id, list);
+        batch = await this.updateFile(id, list, batch);
       }
     }
     else{
-      (currentValue == false) ? newValue = true : newValue = false;
-      console.log(newValue);
-      list.items.getById(itemID).update({ ExternalSite: newValue }, "*", entityTypeFullName).then(b => {
+      // console.log(this._selectedKey.key != 'No');
+      // console.log(newValue);
+      list.items.getById(itemID).inBatch(batch).update({ ExternalSite: newValue }, "*", entityTypeFullName).then(b => {
         console.log(b);
       });
     }
+    return batch;
+  }
+
+  private async updateFolder(itemID: any, list: any, batch: SPBatch){
+    const entityTypeFullName = await list.getListItemEntityTypeFullName();
+    const parser = new JSONParser();
+    let fileType = await list.items.getById(itemID).get(parser);
+    let newValue =(this._selectedKey.key != 'No');
+    if(fileType.FileSystemObjectType == 1){
+      // console.log(this._selectedKey.key != 'No');
+      console.log('Folder');
+      list.items.getById(itemID).inBatch(batch).update({ ExternalSite: newValue }, "*", entityTypeFullName).then(b => {
+        console.log(b);
+      });
+    }
+    return batch;
   }
 
   private async updateListItems(Rows: ReadonlyArray<RowAccessor>) {
     // Update list item here
+    if(this._selectedKey == undefined){
+      return;
+    }
+    console.log(this._selectedKey);
     let list = sp.web.lists.getByTitle("Documents");
+    let batch = sp.web.createBatch();
     list.fields.getByTitle('External Site').update({
       ReadOnlyField: false
     });
     for(let item of Rows){
       let itemID =  item.getValueByName('ID');
-      this.updateFile(itemID, list);
+      batch = await this.updateFolder(itemID, list, batch);
     }
     list.fields.getByTitle('External Site').update({
       ReadOnlyField: true
     });
+    await batch.execute();
     console.log("Done");
   }
 
   @override
   public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
-    let newValue: boolean = false;
     // console.log('ROW:', event.selectedRows[0].getValueByName('ExternalSite'));
     switch (event.itemId) {
       case 'COMMAND_1':
-        this.updateListItems(event.selectedRows);
-        Dialog.alert(`External Sync Updated`); //.then(() => {location.reload()});
+        const diag:  OptionDialog = new OptionDialog();
+        diag.show().then(()=>{
+          this._selectedKey = diag.selectedKey;
+          this.updateListItems(event.selectedRows);
+        });
+
         break;
       default:
         throw new Error('Unknown command');
